b4b4d77ab699429c41adec961441d259dcedd1c8
KeerthikaReddyP <keerthikareddy267@gmail.com>
2024-04-26 20:52:30 +0530

    practiced

---
+++ app.js
@@ -0,0 +1,60 @@
+
+
+
+
+// document.URL;
+
+
+// const todayy=new Date();
+// var christmas=new Date(todayy.getFullYear(),11,25);
+// if(todayy.getMonth()==11 && todayy.getDate()>25){
+//     christmas=new Date(todayy.getFullYear()+1,11,25);
+// }
+// // console.log(todayy.getTime());
+// var oneDay=24*60*60*1000;
+// var diff= christmas.getTime()-todayy.getTime();
+// console.log(Math.ceil(diff/oneDay)+" days left for Christmas");
+
+
+
+// const randNum=Math.random();
+// console.log(Math.round(randNum*10));
+
+
+// window.print();
+//window.open(url,target,features);
+
+
+
+// const datee = new Date();
+// datee.get
+// function getday(d) {
+//   switch (d.getDay()) {
+//     case 1:
+//       return "Monday";
+//     case 2:
+//       return "Tuesday";
+//     case 3:
+//       return "Wednesday";
+//     case 4:
+//       return "Thursday";
+//     case 5:
+//       return "Friday";
+//     case 6:
+//       return "Saturday";
+//     case 0:
+//       return "Sunday";
+
+//     default:
+//       break;
+//   }
+// }
+// console.log("Today is " + getday(datee));
+// console.log(
+//   "TIme " +
+//     datee.getHours() +
+//     " : " +
+//     datee.getMinutes() +
+//     " : " +
+//     datee.getSeconds()
+// );
