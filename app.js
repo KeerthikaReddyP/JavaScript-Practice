2c7db16e3b96c22af2f49382be6f12466cfecbd5
KeerthikaReddyP <keerthikareddy267@gmail.com>
2024-05-30 20:51:36 +0530

    json

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const github_url="https://api.github.com/users/KeerthikaReddyP";
 
 async function handlePromise(){
     const data= await fetch(github_url); //fetch() returns a response
-    console.log(data);
+    //Response.json() returns a promise
+    const jsonData= await data.json();
+    console.log(jsonData);
 }
 handlePromise();
