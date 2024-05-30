ba71dedb6ce010c5f4a3b227373c6af21bbb293e
KeerthikaReddyP <keerthikareddy267@gmail.com>
2024-05-30 20:16:26 +0530

    with async await

--- app.js
+++ app.js
@@ -1,9 +1,18 @@
-//Without async await
 const prom=new Promise(function(resolve,reject){
     resolve("Promise resolved value");
 });
 
-function getData(){
-    prom.then(res=>console.log(res));
+// function getData(){
+//     prom.then(res=>console.log(res));
+// }
+// getData();
+
+
+async function getData(){
+    const val=await prom;
+    console.log(val);
 }
 getData();
+
+//async await combo is used to handle promises.
+//await is a keyword that is used only inside async function
