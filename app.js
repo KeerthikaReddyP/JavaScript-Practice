274de8091abe56457a78f4f4f8b8d83758bc058e
KeerthikaReddyP <keerthikareddy267@gmail.com>
2024-05-30 20:29:41 +0530

    multiple promises

--- app.js
+++ app.js
@@ -1,17 +1,23 @@
-const p=new Promise(function(resolve,reject){
+const p1=new Promise(function(resolve,reject){
     setTimeout(function(){
-        resolve("Promise resolved");
+        resolve("Promise-1 resolved");
     },5000);
+});
+
+const p2=new Promise(function(resolve,reject){
+    setTimeout(function(){
+        resolve("Promise-2 resolved");
+    },3000);
 });
 
 async function handlePromise(){
     //JS Engine seems to wait till promise is settled
-    const val1=await p;
+    const val1=await p1;
     console.log("Namaste promise");
     console.log(val1);
 
-    const val2=await p;
-    console.log("Namaste promise2");
+    const val2=await p2;
+    console.log("Namaste promise");
     console.log(val2);
 }
 handlePromise();
