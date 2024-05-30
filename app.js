fd1b1cc54d4c02037fa43ef4469bdaca2b3ec990
KeerthikaReddyP <keerthikareddy267@gmail.com>
2024-05-30 19:44:50 +0530

    Creating our own promise

--- app.js
+++ app.js
@@ -1,5 +1,29 @@
-const github_url="https://api.github.com/users/KeerthikaReddyP";
+const cart=["shoes","bags","lipstick"];
 
-const user=fetch(github_url);
-console.log(user);
-user.then(res=>console.log(res));
+function createOrder(cart){
+    function validateCart(cart){
+        //code to validate
+        return true;
+    }
+    return new Promise((resolve,reject)=>{
+
+        //validate cart
+        if(!validateCart(cart)){
+            const err=new Error("Cart is not valid!");
+            reject(err);
+        }
+
+        //createOrder
+        //generate order ID
+        const orderId="12345";
+        if(orderId) resolve(orderId);
+    });
+}
+
+createOrder(cart)
+    .then(function(orderId){
+        console.log(orderId);
+    })
+    .catch(function(err){
+        console.log(err);
+    });
