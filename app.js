0afd4fb6cb3d86f8c4de95857032dc1629fa1d71
KeerthikaReddyP <keerthikareddy267@gmail.com>
2024-05-30 21:23:21 +0530

    Promise.allSettled (All success case)

--- app.js
+++ app.js
@@ -9,11 +9,11 @@ const p2=new Promise((resolve,reject)=>{
 });
 
 const p3=new Promise((resolve,reject)=>{
-    // setTimeout(()=>resolve("P3 Success"), 3000);
-    setTimeout(()=>reject("P3 Failed"), 3000);
+    setTimeout(()=>resolve("P3 Success"), 3000);
+    // setTimeout(()=>reject("P3 Failed"), 3000);
 });
 
-
-Promise.all([p1,p2,p3])
+//Prmise.allSettled returns object with status and value
+Promise.allSettled([p1,p2,p3])
     .then(res=>console.log(res))
     .catch(err=>console.error(err));
