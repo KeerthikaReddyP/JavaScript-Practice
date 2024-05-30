568d6a05c1163ef34c108b7df7f55f8923a4f7c1
KeerthikaReddyP <keerthikareddy267@gmail.com>
2024-05-30 21:21:04 +0530

    Promise.all (All success case)

--- app.js
+++ app.js
@@ -1,24 +1,19 @@
-/*
-Promise APIs:
-    1) Promise.all
-    2) Promise.allSettled
-    3) Promise.race
-    4) Promise.any
-*/
+const p1=new Promise((resolve,reject)=>{
+    setTimeout(()=>resolve("P1 Success"), 5000);
+    // setTimeout(()=>reject("P1 Failed"), 5000);
+});
 
-//Promise.all takes array of promises
-//It'll wait for all the API calls to settle.
-// If all promises are fulfilled, it will return an array of promise values.
-//If any of them failed, it'll return the error and do not wait for others to settle. (Fail fast)
+const p2=new Promise((resolve,reject)=>{
+    setTimeout(()=>resolve("P2 Success"), 1000);
+    // setTimeout(()=>reject("P2 Failed"), 1000);
+});
 
-//Promise.allSettled
-//Same as promise.all for all success case
-//If any of them fails, it still waits for all them to settle and returns an array with respective values and errors
+const p3=new Promise((resolve,reject)=>{
+    setTimeout(()=>resolve("P3 Success"), 3000);
+    // setTimeout(()=>reject("P3 Failed"), 3000);
+});
 
-//Promise.race
-//returns the first settled value
-//Doesn't care whether it is fulfilled or rejected
 
-//Promise.any
-//returns the first fulfilled value (first settled success)
-//If all of them fails, it will give an Aggregate Error (Array of all errors)
+Promise.all([p1,p2,p3])
+    .then(res=>console.log(res))
+    .catch(err=>console.error(err));
