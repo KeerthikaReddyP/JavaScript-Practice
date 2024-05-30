c1f55d8cdfb01c89e393ec33435f8ea7339e5d17
KeerthikaReddyP <keerthikareddy267@gmail.com>
2024-05-30 19:59:11 +0530

    Promise Chaining

--- app.js
+++ app.js
@@ -20,10 +20,13 @@ function createOrder(cart){
     });
 }
 
-createOrder(cart)
-    .then(function(orderId){
-        console.log(orderId);
-    })
-    .catch(function(err){
-        console.log(err);
+function proceedToPayment(orderId){
+    return new Promise((resolve,reject)=>{
+        resolve("Payment succeeded");
     });
+}
+createOrder(cart)
+    .then(orderId=>console.log(orderId))
+    .catch(err=>console.error(err))
+    .then(orderId=>proceedToPayment(orderId))
+    .then(res=>console.log(res));
