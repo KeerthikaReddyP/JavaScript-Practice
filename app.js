30f519ee539696baa6e3b076852aaa91aaca52df
KeerthikaReddyP <keerthikareddy267@gmail.com>
2024-05-30 20:10:57 +0530

    async

--- app.js
+++ app.js
@@ -1,32 +1,11 @@
-const cart=["shoes","bags","lipstick"];
+//async always returns a promise
 
-function createOrder(cart){
-    function validateCart(cart){
-        //code to validate
-        return true;
-    }
-    return new Promise((resolve,reject)=>{
-
-        //validate cart
-        if(!validateCart(cart)){
-            const err=new Error("Cart is not valid!");
-            reject(err);
-        }
-
-        //createOrder
-        //generate order ID
-        const orderId="12345";
-        if(orderId) resolve(orderId);
-    });
+async function getData(){
+    return "Namaste"; //If we do not return a promise, it'll wrap our value into a promise and returns it.
 }
 
-function proceedToPayment(orderId){
-    return new Promise((resolve,reject)=>{
-        resolve("Payment succeeded");
-    });
-}
-createOrder(cart)
-    .then(orderId=>console.log(orderId))
-    .catch(err=>console.error(err))
-    .then(orderId=>proceedToPayment(orderId))
+const dataPromise=getData();
+console.log(dataPromise);
+
+dataPromise
     .then(res=>console.log(res));
