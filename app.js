9eb81416ec94ca236485d661c596f718101b759e
Keerthika Reddy <keerthikareddy267@gmail.com>
2024-11-08 21:48:21 +0530

    Back to functions

--- app.js
+++ app.js
@@ -1,19 +1,14 @@
-const p1=new Promise((resolve,reject)=>{
-    setTimeout(()=>resolve("P1 Success"), 5000);
-    // setTimeout(()=>reject("P1 Failed"), 5000);
-});
+var x=1;
 
-const p2=new Promise((resolve,reject)=>{
-    // setTimeout(()=>resolve("P2 Success"), 1000);
-    setTimeout(()=>reject("P2 Failed"), 1000);
-});
+a();
+b();
+console.log(x);
 
-const p3=new Promise((resolve,reject)=>{
-    setTimeout(()=>resolve("P3 Success"), 3000);
-    // setTimeout(()=>reject("P3 Failed"), 3000);
-});
-
-
-Promise.any([p1,p2,p3])
-    .then(res=>console.log(res))
-    .catch(err=>console.error(err));
+function a(){
+    var x=10;
+    console.log(x);
+}
+function b(){
+    var x=100;
+    console.log(x);
+}
