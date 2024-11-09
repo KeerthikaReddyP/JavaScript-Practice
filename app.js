b5e02772a12a5539c33b7fe7d483eff629e629fb
Keerthika Reddy <keerthikareddy267@gmail.com>
2024-11-09 11:17:21 +0530

    Global object (window===this)

---
+++ app.js
@@ -0,0 +1,23 @@
+/*
+- Whenever JS program is run, a global object is created known as "window".
+- Whenever an execution context is created, 'this' is created.
+    - In the global memory space, 'this' refers to the 'window'.
+- Simply put, Anything which isn't inside a function or block is in the global space.
+*/
+
+var a=10;
+
+function b(){
+    var c=20;
+    console.log(c); //20
+    console.log(this.c); //Undefined
+}
+
+console.log(window.a);
+console.log(a);
+console.log(this.a);
+// window.a === a === this.a
+
+b();
+
+// console.log(c); //Error
