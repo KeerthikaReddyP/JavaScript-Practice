b3205190f374be317aa795968b9337d2547ae697
Keerthika Reddy <keerthikareddy267@gmail.com>
2024-11-09 11:26:05 +0530

    Global and local scope

--- app.js
+++ app.js
@@ -3,6 +3,10 @@
 - Whenever an execution context is created, 'this' is created.
     - In the global memory space, 'this' refers to the 'window'.
 - Simply put, Anything which isn't inside a function or block is in the global space.
+- 1. Global scope (Outside of block {})
+  2. Local scope
+        1. Function scope
+        2. Block scope
 */
 
 var a=10;
