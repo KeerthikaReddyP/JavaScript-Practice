1a537709736b23b9dab02d9bac6f854418dec62d
Keerthika Reddy <keerthikareddy267@gmail.com>
2024-11-09 12:02:36 +0530

    Local scope (Function and Block)

--- app.js
+++ app.js
@@ -1,20 +1,38 @@
-//Global scope
+// Local scope
 
-var a=10;
-let b=20;
-const c=30;
+// Function scope
 
 function abc(){
+    var a=10;
+    let b=20;
+    const c=30;
+
     console.log(a); //10
     console.log(b); //20
     console.log(c); //30
-
-    console.log(window.a); //10
-    console.log(window.b); //undefined
-    console.log(window.c); //undefined
 }
-
 abc();
+console.log(a); //Error : a is not defined (function scope)
 
-// 'var' is globally scoped and attached to the window object.
-// 'let' and 'const' are globally scoped but not attached to the window object.
+
+// Block scope
+function xyz(){
+    if(true){
+        var x=10;
+        let y=20;
+        const z=30;
+    }
+    
+    console.log(x); //10
+    console.log(y); //Error : y is not defined (Block scope)
+    console.log(z);
+}
+xyz();
+console.log(x); // Error : x is not defined (function scope)
+
+
+//'var' variable is function scoped.
+// 'let' and 'const' are block scoped.
+
+// let and const variables cannot be accessed outside the block of where they've been declared.
+// var variables cannot be accessed outside the function they've been declared.
