a1bce73ff8e98e01d8ce8b708a4b1d4c15ba34d9
Keerthika Reddy <keerthikareddy267@gmail.com>
2024-11-09 11:37:14 +0530

    Global scope (var,let,const)

--- app.js
+++ app.js
@@ -1,27 +1,20 @@
-/*
-- Whenever JS program is run, a global object is created known as "window".
-- Whenever an execution context is created, 'this' is created.
-    - In the global memory space, 'this' refers to the 'window'.
-- Simply put, Anything which isn't inside a function or block is in the global space.
-- 1. Global scope (Outside of block {})
-  2. Local scope
-        1. Function scope
-        2. Block scope
-*/
+//Global scope
 
 var a=10;
+let b=20;
+const c=30;
 
-function b(){
-    var c=20;
-    console.log(c); //20
-    console.log(this.c); //Undefined
+function abc(){
+    console.log(a); //10
+    console.log(b); //20
+    console.log(c); //30
+
+    console.log(window.a); //10
+    console.log(window.b); //undefined
+    console.log(window.c); //undefined
 }
 
-console.log(window.a);
-console.log(a);
-console.log(this.a);
-// window.a === a === this.a
+abc();
 
-b();
-
-// console.log(c); //Error
+// 'var' is globally scoped and attached to the window object.
+// 'let' and 'const' are globally scoped but not attached to the window object.
