befdfb2bf67f4a11eadc7f8887cd80c68f54f259
Keerthika Reddy <keerthikareddy267@gmail.com>
2024-11-14 21:36:13 +0530

    memory allocation of let variables

--- app.js
+++ app.js
@@ -4,3 +4,11 @@ console.log(b); // undefined
 console.log(a); //Reference Error : cannot access 'a'  before initialization
 let a=10;
 var b=20;
+
+/*
+    Here, even before any line of code is executed, memory is allocated to both 'a' and 'b'.
+    But they are stored in different memory spaces.
+        - The variable(b) declared using 'var' keyword is attached to the global object.
+        - Variable 'a' which is declared using 'let' keyword is stored in a separate memory space(Script).
+            They are not on the global object.
+*/
