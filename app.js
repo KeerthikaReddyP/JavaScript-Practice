92e267e9a204ee6ae2fa49db9ae1fb72ca4680c1
Keerthika Reddy <keerthikareddy267@gmail.com>
2024-11-15 12:53:58 +0530

    const

--- app.js
+++ app.js
@@ -1,18 +1,15 @@
+//CONST
+/*
+    'const' is very much similar to 'let'.
+    But, it's even more strict.
+        It behaves the same way in hoisting. (stored in a separate memory space. temporal dead zone)
+    In case of 'const', initialization and declaration should be done at the same time.
+*/
+
+let a; // Initialization
+a=10; // Declaration
+console.log(a); //10
 
 
-let a=10;
-{
-    let a=100;
-    var b=30;
-}
-function abc(){
-    var c=100;
-}
-abc();
-console.log(c);
 
-//'var' is function scoped.
-// We cannot access it outside of the function it is been declared in.
-// But if it is declared inside a block, it gets attached to the global object(immediate parent of the block).
-//  (If the block is defined inside a function, var is accessible only inside that function. It wont get attached to global object. It gets attached to the function's local memory space.)
-//    Hence we can access it outside the block in which it is declared.
+const b; //SyntaxError : missing initilizer in const declaration
