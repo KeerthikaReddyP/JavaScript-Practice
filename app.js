f67ad10376fc46b4bb32746576731f71fa6aa8b7
Keerthika Reddy <keerthikareddy267@gmail.com>
2024-11-15 11:36:06 +0530

    Duplicate redeclaration of 'let'

--- app.js
+++ app.js
@@ -1,22 +1,14 @@
+// Duplicate redeclaration of 'let'
 
-// console.log(a); // Reference error : Cannot access 'a' before initialization
-// console.log(b); // undefined
-// console.log(x); // Reference error : 'x' is not defined
+console.log("SOmething"); //Not printed to the console
 
 let a=10;
 
-var b=20;
-
-console.log(window.b); // 20
-console.log(window.a); // undefined
-console.log(window.x); // undefined
+let a=20; // SyntaxError : Identifier 'a' has already been declared.
 
 /*
-    Here, window.b gives 20 because 'b' is attached to the global object(window object) as it is declared using 'var' keyword.
-
-    But, window.a gives undefined. As 'a' is declared using 'let' keyword, it isn't attached to global object.
-    They are maintained in a separate storage - reserved memory space for 'let' and 'const'.
-    
-    So, it treats 'a' like any other variable which is not even defined in the program(just like 'x')
-        and pints undefined.
+    We cannot redeclare let variables. 
+    It gives syntax error.
+    It rejects it upfront. No code is run.
+    (But in reference error scenario, code is executed upto the line where error is present. It stops at that particular line.)
 */
