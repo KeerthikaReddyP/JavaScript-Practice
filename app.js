833c17a0246e41b271ed7b073b537e90def11a49
Keerthika Reddy <keerthikareddy267@gmail.com>
2024-11-15 21:01:06 +0530

    Shadowing

--- app.js
+++ app.js
@@ -1,11 +1,17 @@
-// Block scope
+// SHADOWING
+
+var a=100; // attached to global object
+let b=200; // Stored in Script
 
 {
-    var a=10;
-    let b=20;
+    var a=10; // global object
+    let b=20; // stored in block space
     const c=30;
+
+    console.log(a); // 10
+    console.log(b); // 20
+    console.log(c);
 }
 
-// We cannot access let and const outside the block.
-// But we can access var.
-// bcz, let and const are block scoped and var is function scoped.
+console.log(a); // 10 --> bcz, a in the first line is shadowed by a inside the block
+console.log(b); // 200 --> let is block scoped. so it gets stored in separate memory space.
