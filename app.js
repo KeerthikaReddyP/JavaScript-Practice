5ea470e21e40edf6b0f569e02b43591699cb6c2b
Keerthika Reddy <keerthikareddy267@gmail.com>
2024-11-15 11:39:05 +0530

    var

--- app.js
+++ app.js
@@ -4,11 +4,16 @@ console.log("SOmething"); //Not printed to the console
 
 let a=10;
 
-let a=20; // SyntaxError : Identifier 'a' has already been declared.
+var a=20; // SyntaxError : Identifier 'a' has already been declared.
 
 /*
     We cannot redeclare let variables. 
     It gives syntax error.
     It rejects it upfront. No code is run.
     (But in reference error scenario, code is executed upto the line where error is present. It stops at that particular line.)
+*/
+
+/*
+    Same for 'var'.
+    We cannot use the same name in the same scope again.
 */
