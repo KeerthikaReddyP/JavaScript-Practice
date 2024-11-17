6412737731b1e24a453a67c78087717b7533cf2e
Keerthika Reddy <keerthikareddy267@gmail.com>
2024-11-17 15:17:44 +0530

    mdn docs definition for closure

--- app.js
+++ app.js
@@ -10,3 +10,10 @@ function x(){
 x();
 
 //Function bundled with it's lexical environment is known as closure.
+
+/*
+    A closure is the combination of a function bundled together(enclosed) with
+    references to its surrounding state(the lexical environment).
+
+    A closure gives you access to an outer function's scope from an inner function.
+*/
