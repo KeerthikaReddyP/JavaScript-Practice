0e0af223587b7e6a5579518df73f465652ceaa17
Keerthika Reddy <keerthikareddy267@gmail.com>
2024-11-22 20:27:46 +0530

    Limitations of using callback

--- app.js
+++ app.js
@@ -1,19 +1,26 @@
+const cart = ["shoes", "ribbons", "bottle"];
 
+createOrder(cart, function () {
+  proceedToPayment(orderId);
+});
 
-console.log("Start");
-
-setTimeout(function cb(){
-    console.log("Timer");
-},5000);
-
-
-let start=new Date().getTime();
-let end=start;
-while(end<start+10000){
-    end = new Date().getTime();
-}
-
-console.log("10 sec pause");
-
-
-console.log("End");
+/*
+    Limitations of using callbacks
+        1. Callbak hell 
+        2. Inversion of control
+*/
+/*
+    If we have callback inside a callback inside a callback and so on, it creates callback hell. Pyramid of Doom.
+    - Our code grows horizontally, instead of vertically
+    - It becomes unreadable and unmaintanable
+*/
+/*
+    Inversion of control : 
+        - As we're passing our callback function to another function, we're losing control of it.
+        - We're blindly trusting the function to which we've given our callback function.
+        - createOrder function might be written by some other developer, or some intern
+            and we've give control of our proceedToPayment callback to createOrder.
+            We don't know what's inside createOrder
+            It might not call our proceedToPayment function, or it might call it twice, or it might misuse it
+        - We just don't have the control over our callback function.
+*/
