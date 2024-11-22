81221ec33e67e4564e1bc8eda35870d7e90287a6
Keerthika Reddy <keerthikareddy267@gmail.com>
2024-11-22 22:28:43 +0530

    Promise chaining

--- app.js
+++ app.js
@@ -1,30 +1,40 @@
-// PROMISE
+// PROMISE CHAINING
 
-const GITHUB_API_URL="https://api.github.com/users/KeerthikaReddyP";
+//The problem with using callbacks is callback hell or pyramid of doom.
 
-const promise= fetch(GITHUB_API_URL);
+createOrder(cart, function () {
+  proceedToPayment(orderId, function () {
+    showOrderSummary(paymentInfo, function () {
+      updateWallet();
+    });
+  });
+});
+// This is not readable and maintainable.
+// As code grows, & if we have even more if,else, for loops, it'll become even more complicated.
+// And we don't know which API is getting called where, and the result.
+// Code is growing horizontally instread of vertical.
 
-console.log(promise);
 
-promise.then(data=>console.log(data));
+// Using promises
 
-// A promise is an object representing eventual completion/failure of any asynchronous operation.
-/*
-    fetch method returns a promise.
-    Here, it is fetching data fro github users api.
-    - As it is an asynchronous operation, we don't know how much it'll take.
-    - By the time it gets any data, it's value is undefined.
-    
-    - This promise , once resolved, gets populated with data.
-    - It has promise state and promise response
+createOrder(cart)
+  .then(function (orderId) {
+    return proceedToPayment(orderId);
+  })
+  .then(function (paymentInfo) {
+    return showOrderSummary(paymentInfo);
+  })
+  .then(function () {
+    return updateWallet();
+  });
+// This is known as promise chaining.
+// One common mistake - not writing return
+// One must return the callback to ensure the flow of promise chain. Else, it will throw error, or stuck somewhere.
 
-    - And once the promise is resolved(success/failure), our callback function which is attached to it using
-        .then() method gets called
-    - Hence, we've control over our code and we know what our callback is upto.
 
-    - Promise only has 3 states. Pending, fulfilled and rejected.
-*/
-/*
-    The response from promise is readable stream. We cannot directly read it. But we can covert it into json.
-*/
-// promises are immutable. We cannot change the data we got from promise.
+// We can also use arrow functions.
+// This will make code even more leaner and readable.
+createOrder(cart)
+  .then(orderId=>proceedToPayment(orderId))
+  .then(paymentInfo=>showOrderSummary(paymentInfo))
+  .then(()=>updateWallet());
