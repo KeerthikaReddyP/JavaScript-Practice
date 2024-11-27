9f5b0948207936afe3a43b1de83fd9bb3cfdb032
Keerthika Reddy <keerthikareddy267@gmail.com>
2024-11-27 09:56:06 +0530

    Promise Chaining

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const cart = ["shoes", "headset", "cake"];
 
 function validateCart(cart) {
-  return false;
+  return true;
 }
 
 function createOrder(cart) {
@@ -24,11 +24,15 @@ function createOrder(cart) {
   return prom;
 }
 
-const pr = createOrder(cart); // This returns a promise
+function proceedToPayment(orderId){
+  return new Promise((resolve,reject)=>resolve("Payment successful"));
+}
 
-pr.then((orderId) => console.log(orderId))
+createOrder(cart)
+  .then((orderId) => {
+    console.log(orderId);
+    return orderId;
+  })
+  .then((orderId) => proceedToPayment(orderId))
+  .then((paymentInfo)=>console.log(paymentInfo))
   .catch((err) => console.log(err.message));
-
-//This will give red color error in console, because we didn't handle it.
-// We should handle errors gracefully.
-// Using .catch method
