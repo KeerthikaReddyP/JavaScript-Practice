8671612b1c38b55ddc76b5b3966c70644391e353
Keerthika Reddy <keerthikareddy267@gmail.com>
2024-11-27 09:32:14 +0530

    Fake timer using setTimeout to reseble realtime async operation

--- app.js
+++ app.js
@@ -1,30 +1,29 @@
-const cart=["shoes","headset","cake"];
+const cart = ["shoes", "headset", "cake"];
 
-function validateCart(cart){
+function validateCart(cart) {
   return true;
 }
 
-function createOrder(cart){
+function createOrder(cart) {
+  const prom = new Promise(function (resolve, reject) {
+    //create order logic
+    //Validate cart
+    //orderId
 
-  const prom=new Promise(function(resolve,reject){
-      //create order logic
-      //Validate cart
-      //orderId
+    if (!validateCart(cart)) {
+      const err = new Error("Cart is not valid");
+      reject(err);
+    }
 
-      if(!validateCart(cart)){
-        const err=new Error("Cart is not valid");
-        reject(err);
-      }
-      
-      const orderId=123456;
-      if(orderId){
-        resolve(orderId);
-      }
+    const orderId = 123456;
+    if (orderId) {
+      setTimeout(() => resolve(orderId), 5000);
+    }
   });
 
   return prom;
 }
 
-const pr=createOrder(cart); // This returns a promise
+const pr = createOrder(cart); // This returns a promise
 
-pr.then(orderId=>console.log(orderId));
+pr.then((orderId) => console.log(orderId));
