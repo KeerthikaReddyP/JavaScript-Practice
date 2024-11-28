42b7de7a753fa24a7f05ce020422935708a3a497
Keerthika Reddy <keerthikareddy267@gmail.com>
2024-11-28 18:25:43 +0530

    Promise.all()

--- app.js
+++ app.js
@@ -1,44 +1,26 @@
-const cart = ["shoes", "headset", "cake"];
+// PROMISE APIs
 
-function validateCart(cart) {
-  return true;
-}
+const p1 = new Promise((resolve, reject) => {
+  setTimeout(() => resolve("p1 resolved"), 3000);
+});
 
-function createOrder(cart) {
-  const prom = new Promise(function (resolve, reject) {
-    //create order logic
-    //Validate cart
-    //orderId
+const p2=new Promise((resolve,reject)=>{
+  setTimeout(()=>resolve("p2 resolved"),1000);
+});
 
-    if (!validateCart(cart)) {
-      const err = new Error("Cart is not valid");
-      reject(err);
-    }
+const p3=new Promise((resolve,reject)=>{
+  setTimeout(()=>resolve("p3 resolved"),5000);
+});
 
-    const orderId = 123456;
-    if (orderId) {
-      setTimeout(() => resolve(orderId), 5000);
-    }
-  });
-
-  return prom;
-}
-
-function proceedToPayment(orderId){
-  return new Promise((resolve,reject)=>resolve("Payment successful"));
-}
-
-createOrder(cart)
-  .then((orderId) => {
-    console.log(orderId);
-    return orderId;
-  })
-  .then((orderId) => proceedToPayment(orderId))
-  .then((paymentInfo)=>console.log(paymentInfo))
-  .catch((err) => console.log(err.message))
-  .then(()=>console.log("I'll execute no matter what!"));
+Promise.all([p1,p2,p3])
+  .then((values)=>console.log(values));
 
 
+/*
+  Promise.all takes an iterable (array in an iterable, we mostly use arrays)
+  and returns a promise 
+  whose value is an array of all the success values of all the promises sent.
 
-// .catch method will handle errors only above it
-// We can still move forward to next step here, even if there's an error in prev step
+  This will return only if all promises are resolved.
+  And only after all promises are resolved. (After 5 secs in above case)
+*/
