222b2a330f4a313dca7f6cce3a9afcf2017e4db0
Keerthika Reddy <keerthikareddy267@gmail.com>
2024-12-02 21:26:08 +0530

    'this' keyword

--- app.js
+++ app.js
@@ -1,18 +1,31 @@
-// PROMISE APIs
+"use strict"
+// 'this' keyword
 
-const p1 = new Promise((resolve, reject) => {
-  setTimeout(() => reject("p1 rejected"), 3000);
-});
+// In global space, this keyword points to global object(Global object is window incase of browsers)
+console.log(this);
 
-const p2=new Promise((resolve,reject)=>{
-  setTimeout(()=>resolve("p2 resolved"),1000);
-});
 
-const p3=new Promise((resolve,reject)=>{
-  setTimeout(()=>resolve("p3 resolved"),5000);
-});
+// Inside functions
+function x(){
+  console.log(this); // undefined in strict mode. window in non strict mode
+}
 
-Promise.allSettled([p1,p2,p3])
-  .then(val=>console.log(val));
+// this substitution
+// If the value of this is undefined or null, it'll be substituted by global this(i.e., window object)
+// in non-strict mode
 
-//Returns array of hree responses, doesn't matter fulfilled or rejected
+
+// Based on how the function is called
+x(); // undefined
+window.x(); // window
+
+
+// Inside object's method
+const obj1={
+  a: 10,
+  x:function(){
+    console.log(this);
+  }
+};
+
+obj1.x(); // refers to the object(obj1) itself
