29e154f316433f1370d64bcf45509d8ccdc58f85
Keerthika Reddy <keerthikareddy267@gmail.com>
2024-12-03 07:16:46 +0530

    .call method

--- app.js
+++ app.js
@@ -1,31 +1,27 @@
 "use strict"
 // 'this' keyword
 
-// In global space, this keyword points to global object(Global object is window incase of browsers)
-console.log(this);
+// call method
 
+const student1={
+  name:"Keerthika",
+  printName:function(){
+    console.log(this.name);
+  }
+}
+student1.printName(); // Keerthika
 
-// Inside functions
-function x(){
-  console.log(this); // undefined in strict mode. window in non strict mode
+const student2={
+  name:"Dileep",
 }
 
-// this substitution
-// If the value of this is undefined or null, it'll be substituted by global this(i.e., window object)
-// in non-strict mode
+// Now can we use printName method for the second object(student2)?
+// Yes.
 
+student1.printName.call(student2); // Dileep
 
-// Based on how the function is called
-x(); // undefined
-window.x(); // window
-
-
-// Inside object's method
-const obj1={
-  a: 10,
-  x:function(){
-    console.log(this);
-  }
-};
-
-obj1.x(); // refers to the object(obj1) itself
+// Here, we can do this by overriding the 'this' value of the function printName.
+// .call method takes the value of 'this'
+// We're passing student2 to .call method, so the value of 'this' will refer to student2.
+// Hence Dileep is printed
+// Method sharing is done
