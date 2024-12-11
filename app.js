00e20b74b2a8b04f45ba6beb28a733a875093de5
Keerthika Reddy <keerthikareddy267@gmail.com>
2024-12-11 22:19:13 +0530

    Smoll question on asyncronous programming i JavaScript

--- app.js
+++ app.js
@@ -1,27 +1,17 @@
-"use strict"
-// 'this' keyword
+// What's the output of this code?
 
-// call method
+const promise = new Promise((resolve, reject) => {
+  console.log(1);
+  setTimeout(() => {
+    console.log("timerStart");
+    resolve("success");
+    console.log("timerEnd");
+  }, 0);
+  console.log(2);
+});
 
-const student1={
-  name:"Keerthika",
-  printName:function(){
-    console.log(this.name);
-  }
-}
-student1.printName(); // Keerthika
+promise.then((res) => {
+  console.log(res);
+});
 
-const student2={
-  name:"Dileep",
-}
-
-// Now can we use printName method for the second object(student2)?
-// Yes.
-
-student1.printName.call(student2); // Dileep
-
-// Here, we can do this by overriding the 'this' value of the function printName.
-// .call method takes the value of 'this'
-// We're passing student2 to .call method, so the value of 'this' will refer to student2.
-// Hence Dileep is printed
-// Method sharing is done
+console.log(4);
