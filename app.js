0ee349ab608e44ebe4f505722ef04c62c6cb0404
Keerthika Reddy <keerthikareddy267@gmail.com>
2024-12-31 22:07:04 +0530

    Setting up debouncing logic

--- app.js
+++ app.js
@@ -6,3 +6,12 @@ const getData=()=>{
   // Makes an API call to get data
   console.log("Fetching Data", ++counter);
 }
+
+function debounce(fn, delay){
+  // Debouncing logic (returns a function)
+  return function(){
+    
+  }
+};
+
+const betterFunction=debounce(getData,300);
