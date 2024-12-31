afa25cacc5e482cb5d391569e4a149a27f54433e
Keerthika Reddy <keerthikareddy267@gmail.com>
2024-12-31 21:58:50 +0530

    Setting up debouncing

--- app.js
+++ app.js
@@ -1,26 +1,6 @@
-// What's the output of this code?
+// Debouncing
 
-const promise = new Promise((resolve, reject) => {
-  console.log(1);
-  setTimeout(() => {
-    console.log("timerStart");
-    resolve("success");
-    console.log("timerEnd");
-  }, 0);
-  console.log(2);
-});
-
-promise.then((res) => {
-  console.log(res);
-});
-
-console.log(4);
-
-/*
-  1
-  2
-  4
-  timerStart
-  timerEnd
-  success
-*/
+const getData=()=>{
+  // Makes an API call to get data
+  console.log("Fetching Data");
+}
