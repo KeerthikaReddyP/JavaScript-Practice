e4b625cb663713b01af07066c32c8756a0742be2
Keerthika Reddy <keerthikareddy267@gmail.com>
2026-01-22 14:33:22 +0530

    Expense tracker

--- app.js
+++ app.js
@@ -1,21 +1,70 @@
-// Debouncing
+let expenses = JSON.parse(localStorage.getItem("expenses")) || [];
 
-let counter=0;
+// window.onload = renderExpenses;
+document.addEventListener("DOMContentLoaded", renderExpenses);
 
-const getData=()=>{
-  // Makes an API call to get data
-  console.log("Fetching Data", ++counter);
+
+function addExpense() {
+  const amount = document.getElementById("amount").value;
+  const description = document.getElementById("description").value;
+  const category = document.getElementById("category").value;
+
+  if (!amount || !description) {
+    alert("Please enter amount and description");
+    return;
+  }
+
+  const expense = {
+    id: Date.now(),
+    amount,
+    description,
+    category,
+  };
+
+  expenses.push(expense);
+  updateLocalStorage();
+  renderExpenses();
+
+  // Clear inputs
+  document.getElementById("amount").value = "";
+  document.getElementById("description").value = "";
 }
 
-function debounce(fn, delay){
-  // Debouncing logic (returns a function)
-  let timer;
-  return function(...args){
-    clearTimeout(timer);
-    timer=setTimeout(()=>{
-      fn.apply(this,args);
-    },delay);
-  }
-};
+function renderExpenses() {
+  const list = document.getElementById("expenseList");
+  list.innerHTML = "";
 
-const betterFunction=debounce(getData,300);
+  expenses.forEach((exp) => {
+    const li = document.createElement("li");
+    li.innerHTML = `
+          ${exp.amount} - ${exp.category} - ${exp.description}
+          <button onclick="deleteExpense(${exp.id})">Delete Expense</button>
+          <button onclick="editExpense(${exp.id})">Edit Expense</button>
+        `;
+    list.appendChild(li);
+  });
+}
+
+function deleteExpense(id) {
+  expenses = expenses.filter((exp) => exp.id !== id);
+  updateLocalStorage();
+  renderExpenses();
+}
+
+function editExpense(id) {
+  const expense = expenses.find((exp) => exp.id === id);
+
+  const newAmount = prompt("Enter new amount", expense.amount);
+  const newDescription = prompt("Enter new description", expense.description);
+
+  if (newAmount && newDescription) {
+    expense.amount = newAmount;
+    expense.description = newDescription;
+    updateLocalStorage();
+    renderExpenses();
+  }
+}
+
+function updateLocalStorage() {
+  localStorage.setItem("expenses", JSON.stringify(expenses));
+}
